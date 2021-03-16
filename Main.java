c4285086c5e94e6b6c3a5373e9b30a25f3375d5c
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-03-15 22:33:58 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
 class Main {
   public static void main(String[] args) {
-    //add test cases 
+  ReverseNumber r = new ReverseNumber(234);
+
+System.out.println(r.getNumber() + " reversed is " + r.getReverse() + " ");
+
+r.setNumber(10000);
+
+System.out.println(r.getNumber() + " reversed is " + r.getReverse() + " ");
+
+r.setNumber(111);
+
+System.out.println(r.getNumber() + " reversed is " + r.getReverse() + " ");
+
+r.setNumber(9005);
+
+System.out.println(r.getNumber() + " reversed is " + r.getReverse() + " ");
+
+r.setNumber(84645);
+
+System.out.println(r.getNumber() + " reversed is " + r.getReverse() + " ");
+
+r.setNumber(8547);
+
+System.out.println(r.getNumber() + " reversed is " + r.getReverse() + " ");
+
+r.setNumber(123456789);
+
+System.out.println(r.getNumber() + " reversed is " + r.getReverse());  
   }
 }
